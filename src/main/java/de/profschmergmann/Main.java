0df5b09d272eff4959026e4722730efa3e496a79
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-07 12:18:12 +0100

    Finished Day5.

--- src/main/java/de/profschmergmann/Main.java
+++ src/main/java/de/profschmergmann/Main.java
@@ -4,16 +4,18 @@ import de.profschmergmann.day1.Day1;
 import de.profschmergmann.day2.Day2;
 import de.profschmergmann.day3.Day3;
 import de.profschmergmann.day4.Day4;
+import de.profschmergmann.day5.Day5;
 
 public class Main {
 
-    public static Day[] days = {new Day1(), new Day2(), new Day3(), new Day4()};
+//    public static Day[] days = {new Day1(), new Day2(), new Day3(), new Day4(),
+//            new Day5()};
 
     public static void main(String[] args) {
 //        for (Day day : days) {
 //            printResults(day);
 //        }
-        printResults(new Day4());
+        printResults(new Day5());
     }
 
     public static void printResults(Day day) {
