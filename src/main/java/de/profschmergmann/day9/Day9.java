d318e9da6ac9664a4914506149d4d9d014649956
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-08 11:10:12 +0100

    Prepared for Day9.

---
+++ src/main/java/de/profschmergmann/day9/Day9.java
@@ -0,0 +1,25 @@
+package de.profschmergmann.day9;
+
+import de.profschmergmann.Day;
+
+public class Day9 extends Day {
+
+    public Day9() {
+        super(9);
+    }
+
+    @Override
+    public String resultPartOne() {
+        return null;
+    }
+
+    @Override
+    public String resultPartTwo() {
+        return null;
+    }
+
+    @Override
+    public int number() {
+        return 9;
+    }
+}
