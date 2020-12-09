dc7b49cfac5db795309b4cf4025640b03b0d5db6
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-09 08:11:50 +0100

    Prepared for Day10

--- src/main/java/de/profschmergmann/Main.java
+++ src/main/java/de/profschmergmann/Main.java
@@ -1,6 +1,7 @@
 package de.profschmergmann;
 
 import de.profschmergmann.day1.Day1;
+import de.profschmergmann.day10.Day10;
 import de.profschmergmann.day2.Day2;
 import de.profschmergmann.day3.Day3;
 import de.profschmergmann.day4.Day4;
@@ -13,7 +14,7 @@ import de.profschmergmann.day9.Day9;
 public class Main {
 
     public static void main(String[] args) {
-        printResults(new Day9());
+        printResults(new Day10());
     }
 
     public static void printResults(Day day) {
@@ -24,7 +25,8 @@ public class Main {
 
     private static Day[] createDayArray() {
         return new Day[]{new Day1(), new Day2(), new Day3(), new Day4(),
-                new Day5(), new Day6(), new Day7(), new Day8()};
+                new Day5(), new Day6(), new Day7(), new Day8(), new Day9(),
+                new Day10()};
     }
 
 }
---
+++ src/main/java/de/profschmergmann/day10/Day10.java
@@ -0,0 +1,24 @@
+package de.profschmergmann.day10;
+
+import de.profschmergmann.Day;
+
+public class Day10 extends Day {
+    public Day10() {
+        super(10);
+    }
+
+    @Override
+    public String resultPartOne() {
+        return null;
+    }
+
+    @Override
+    public String resultPartTwo() {
+        return null;
+    }
+
+    @Override
+    public int number() {
+        return 10;
+    }
+}
