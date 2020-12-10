f9ebebee377e900b5a18d8fe661b485e3f4b5739
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-10 09:37:32 +0100

    Finished Day10 Part 1.

--- src/main/java/de/profschmergmann/day10/Day10.java
+++ src/main/java/de/profschmergmann/day10/Day10.java
@@ -2,14 +2,48 @@ package de.profschmergmann.day10;
 
 import de.profschmergmann.Day;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day10 extends Day {
+
+    private final List<Integer> sortedJoltAdapters;
+
     public Day10() {
         super(10);
+        this.sortedJoltAdapters = new ArrayList<>();
+        // Add 0 to the beginning
+        this.sortedJoltAdapters.add(0);
+        this.input.stream()
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .boxed()
+                .forEach(this.sortedJoltAdapters::add);
+        // Add the device built in adapter
+        this.sortedJoltAdapters.add(this.sortedJoltAdapters.get(this.sortedJoltAdapters.size() - 1) + 3);
     }
 
     @Override
     public String resultPartOne() {
-        return null;
+        int oneJoltDifference = 0;
+        int twoJoltDifference = 0;
+        int threeJoltDifference = 0;
+        for (int i = 1; i < this.sortedJoltAdapters.size(); i++) {
+            switch (this.sortedJoltAdapters.get(i) - this.sortedJoltAdapters.get(i - 1)) {
+                case 1:
+                    oneJoltDifference++;
+                    break;
+                case 2:
+                    twoJoltDifference++;
+                    break;
+                case 3:
+                    threeJoltDifference++;
+                    break;
+                default:
+                    throw new IllegalArgumentException("There cannot be larger jolt differences than 1, 2 or 3.");
+            }
+        }
+        return String.valueOf(oneJoltDifference * threeJoltDifference);
     }
 
     @Override
