242f543d249b0590094c82347ea3b223df64b976
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-11 10:45:42 +0100

    Prepared for Day12.

---
+++ src/main/java/de/profschmergmann/day12/Day12.java
@@ -0,0 +1,24 @@
+package de.profschmergmann.day12;
+
+import de.profschmergmann.Day;
+
+public class Day12 extends Day {
+    public Day12() {
+        super(12);
+    }
+
+    @Override
+    public String resultPartOne() {
+        return null;
+    }
+
+    @Override
+    public String resultPartTwo() {
+        return null;
+    }
+
+    @Override
+    public int number() {
+        return 12;
+    }
+}
