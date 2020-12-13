4ba928280fdaa7ff6720eb78f0cbbbb733fefad3
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-13 12:24:52 +0100

    Prepared for day13

--- src/main/java/de/profschmergmann/Main.java
+++ src/main/java/de/profschmergmann/Main.java
@@ -4,6 +4,7 @@ import de.profschmergmann.day1.Day1;
 import de.profschmergmann.day10.Day10;
 import de.profschmergmann.day11.Day11;
 import de.profschmergmann.day12.Day12;
+import de.profschmergmann.day13.Day13;
 import de.profschmergmann.day2.Day2;
 import de.profschmergmann.day3.Day3;
 import de.profschmergmann.day4.Day4;
@@ -16,7 +17,7 @@ import de.profschmergmann.day9.Day9;
 public class Main {
 
     public static void main(String[] args) {
-        printResults(new Day12());
+        printResults(new Day13());
     }
 
     public static void printResults(Day day) {
@@ -28,7 +29,7 @@ public class Main {
     private static Day[] createDayArray() {
         return new Day[]{new Day1(), new Day2(), new Day3(), new Day4(),
                 new Day5(), new Day6(), new Day7(), new Day8(), new Day9(),
-                new Day10(), new Day11(), new Day12()};
+                new Day10(), new Day11(), new Day12(), new Day13()};
     }
 
 }
---
+++ src/main/java/de/profschmergmann/day13/Day13.java
@@ -0,0 +1,25 @@
+package de.profschmergmann.day13;
+
+import de.profschmergmann.Day;
+
+public class Day13 extends Day {
+
+    public Day13() {
+        super(13);
+    }
+
+    @Override
+    public String resultPartOne() {
+        return null;
+    }
+
+    @Override
+    public String resultPartTwo() {
+        return null;
+    }
+
+    @Override
+    public int number() {
+        return 13;
+    }
+}
