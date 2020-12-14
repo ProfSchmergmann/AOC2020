1c6adee59a692e3291330795faf6c0707dc240f9
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-14 10:14:45 +0100

    Solved part one.

--- src/main/java/de/profschmergmann/Main.java
+++ src/main/java/de/profschmergmann/Main.java
@@ -5,6 +5,7 @@ import de.profschmergmann.day10.Day10;
 import de.profschmergmann.day11.Day11;
 import de.profschmergmann.day12.Day12;
 import de.profschmergmann.day13.Day13;
+import de.profschmergmann.day14.Day14;
 import de.profschmergmann.day2.Day2;
 import de.profschmergmann.day3.Day3;
 import de.profschmergmann.day4.Day4;
@@ -17,7 +18,7 @@ import de.profschmergmann.day9.Day9;
 public class Main {
 
     public static void main(String[] args) {
-        printResults(new Day13());
+        printResults(new Day14());
     }
 
     public static void printResults(Day day) {
@@ -29,7 +30,7 @@ public class Main {
     private static Day[] createDayArray() {
         return new Day[]{new Day1(), new Day2(), new Day3(), new Day4(),
                 new Day5(), new Day6(), new Day7(), new Day8(), new Day9(),
-                new Day10(), new Day11(), new Day12(), new Day13()};
+                new Day10(), new Day11(), new Day12(), new Day13(), new Day14()};
     }
 
 }
