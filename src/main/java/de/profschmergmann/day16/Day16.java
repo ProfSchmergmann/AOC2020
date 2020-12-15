5eb7c2fca6571b1afafdece21ce3fb34f63b9c5d
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2020-12-15 11:34:24 +0100

    Prepared for Day16

---
+++ src/main/java/de/profschmergmann/day16/Day16.java
@@ -0,0 +1,24 @@
+package de.profschmergmann.day16;
+
+import de.profschmergmann.Day;
+
+public class Day16 extends Day {
+    public Day16() {
+        super(16);
+    }
+
+    @Override
+    public String resultPartOne() {
+        return null;
+    }
+
+    @Override
+    public String resultPartTwo() {
+        return null;
+    }
+
+    @Override
+    public int number() {
+        return 16;
+    }
+}
